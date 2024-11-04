6f9cd32945e339a95fda1ac6f077faf358384655
HassanMunene <awanzihassan@gmail.com>
2024-11-04 09:30:41 +0300

    still working on the header section

--- js/script.js
+++ js/script.js
@@ -2,45 +2,17 @@
                     NAVIGATION BAR SECTION
 ==============================================================================================*/
 document.addEventListener('DOMContentLoaded', function() {
-    let navbar = document.querySelector('.navbar');
-    const menu_btn = document.getElementById('menu-btn');
-
-    let search_form = document.querySelector('.search-form');
-    const search_btn = document.getElementById('search-btn');
-
-    let contact_info = document.querySelector('.contact-info');
-    const info_btn = document.getElementById('info-btn');
-    const close_contact_info = document.getElementById('close-contact-info');
-
-
-    menu_btn.addEventListener('click', function() {
-        navbar.classList.toggle('active');
-        search_form.classList.remove('active');
-        contact_info.classList.remove('active');
-    })
-
-    search_btn.addEventListener('click', function() {
-        search_form.classList.toggle('active');
-        navbar.classList.remove('active');
-        contact_info.classList.remove('active');
-    })
-
-    info_btn.addEventListener('click', function() {
-        contact_info.classList.add('active');
-        navbar.classList.remove('active');
-        search_form.classList.remove('active');
-    })
-
-    close_contact_info.addEventListener('click', function() {
-        contact_info.classList.remove('active');
-        navbar.classList.remove('active');
-        search_form.classList.remove('active');
-    })
-    window.onscroll = () => {
-        navbar.classList.remove('active');
-        search_form.classList.remove('active');
-        contact_info.classList.remove('active');
-    }
+    document.getElementById('open-menu-btn').addEventListener('click', function() {
+        const mobileMenu = document.querySelector('.mobile-menu');
+        mobileMenu.classList.add('active');
+        mobileMenu.classList.remove('inactive'); // Make it interactive
+    });
+    
+    document.getElementById('close-menu-btn').addEventListener('click', function() {
+        const mobileMenu = document.querySelector('.mobile-menu');
+        mobileMenu.classList.remove('active');
+        mobileMenu.classList.add('inactive'); // Prevent interaction while hidden
+    });
 
     var swiper = new Swiper(".home-slider", {
         loop: true,
@@ -71,7 +43,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 })
-
 
 /*=============================================================================================
                             SERVICES SECTION JS
