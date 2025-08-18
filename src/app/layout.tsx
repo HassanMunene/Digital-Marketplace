00ff3c2cc0aaf33c5bb20c3e7525d596f8176d29
HassanMunene <awanzihassan@gmail.com>
2025-08-18 14:36:57 +0300

    we have completed the desktop large navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Navbar from "@/components/Navbar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body className={`relative h-full font-sans ${geistSans.variable} ${geistMono.variable} antialiased`}>
         <main className="relative flex flex-col min-h-screen">
           <div className="flex-grow flex-1">
+            <Navbar />
             {children}
           </div>
         </main>
