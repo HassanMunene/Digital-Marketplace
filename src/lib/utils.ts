da0c96c4e3bdfbe35c74b5f48c07646640886ec8
HassanMunene <awanzihassan@gmail.com>
2025-08-19 14:38:56 +0300

    Added a utility helper to format prices

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+type Options = {
+  currency?: "KES" | "KSH";
+  notation?: Intl.NumberFormatOptions["notation"]
+}
+export function formatPrice(price: number | string, options: Options = {}) {
+  const { currency = "KSH", notation = "compact" } = options;
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat('en-us', {
+    style: "currency",
+    currency: currency,
+    notation: notation,
+    maximumFractionDigits: 2
+  }).format(numericPrice);
+}
